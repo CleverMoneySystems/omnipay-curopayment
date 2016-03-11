7fb987003441f6c3a3eed60eb775f5abced184ac
Bram van Eijk <bram@clevercoin.com>
2016-03-11 16:17:58 +0100

    Remove Error message

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -10,7 +10,6 @@ namespace Omnipay\Curo\Message;
 use Omnipay\Common\CreditCard;
 use Omnipay\Common\Exception\InvalidRequestException;
 use Guzzle\Http\Exception\BadResponseException;
-use Omnipay\Curo\Message\Error;
 
 /**
  * PurchaseRequest class - it registers a transaction
